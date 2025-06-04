1e420394286340a031eba0958222c6547541385e
Brian <163011832+brx2b@users.noreply.github.com>
2025-06-04 18:19:39 -0400

    Add files via upload

--- MasterBike_Landing_page/scriptPrincipal.js
+++ MasterBike_Landing_page/scriptPrincipal.js
@@ -3,17 +3,17 @@ let pass1="123";
 function iniciar(){
     try {
         if(document.getElementById("nicknameU").value===user1 && document.getElementById("contraU").value===pass1){
-            console.log("SESIÓN CORRECTA")
+            console.log("Datos correctos")
             document.getElementById("errorS").innerHTML='<p style="color: green;">DATOS CORRECTOS</p>';
         }else if(document.getElementById("nicknameU").value.trim()==="" || document.getElementById("contraU").value.trim()===""){
             console.log("Completa los campos")
             document.getElementById("errorS").innerHTML='<p style="color: red;">COMPLETA AMBOS CAMPOS</p>';
         }
         else{
-            console.log("WTF")
+            console.log("Datos incorrectos ingresados")
             document.getElementById("errorS").innerHTML='<p style="color: red;">DATOS INCORRECTOS</p>';
         }
     } catch (error) {
-        console.log("Se ha producito un error")
+        console.log("Se ha producido un error")
     }
 };
