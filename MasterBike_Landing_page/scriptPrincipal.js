e9efc95d92b962f2153c46c6c230eddf31f4b90e
Brian <163011832+brx2b@users.noreply.github.com>
2025-06-04 18:05:24 -0400

    Inicio sesión agregado sin DB
    Archivos principales de inicio de sesión sin base de datos

---
+++ MasterBike_Landing_page/scriptPrincipal.js
@@ -0,0 +1,19 @@
+let user1="brx2b";
+let pass1="123";
+function iniciar(){
+    try {
+        if(document.getElementById("nicknameU").value===user1 && document.getElementById("contraU").value===pass1){
+            console.log("SESIÓN CORRECTA")
+            document.getElementById("errorS").innerHTML='<p style="color: green;">DATOS CORRECTOS</p>';
+        }else if(document.getElementById("nicknameU").value.trim()==="" || document.getElementById("contraU").value.trim()===""){
+            console.log("Completa los campos")
+            document.getElementById("errorS").innerHTML='<p style="color: red;">COMPLETA AMBOS CAMPOS</p>';
+        }
+        else{
+            console.log("WTF")
+            document.getElementById("errorS").innerHTML='<p style="color: red;">DATOS INCORRECTOS</p>';
+        }
+    } catch (error) {
+        console.log("Se ha producito un error")
+    }
+};
