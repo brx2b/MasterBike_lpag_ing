bd319349458b2db98aef90893ba5b3a9635eef61
FabianReyes02 <fa.reyeso@duocuc.cl>
2025-06-13 10:29:51 -0400

    Se creo la renta

--- scriptPrincipal.js
+++ scriptPrincipal.js
@@ -1,21 +1,25 @@
-let user1="brx2b";
-let pass1="123";
-function iniciar(){
+
+let user1 = "brx2b";
+let pass1 = "123";
+
+function iniciar() {
     try {
-        if(document.getElementById("nicknameU").value===user1 && document.getElementById("contraU").value===pass1){
-            console.log("Datos correctos")
-            document.getElementById("errorS").innerHTML='<p style="color: green;">DATOS CORRECTOS</p>';
-            window.alert("Sesion iniciada ¡Bienvenido!")
-            window.location.href='Inicio/inicio.html';
-        }else if(document.getElementById("nicknameU").value.trim()==="" || document.getElementById("contraU").value.trim()===""){
-            console.log("Completa los campos")
-            document.getElementById("errorS").innerHTML='<p style="color: red;">COMPLETA AMBOS CAMPOS</p>';
-        }
-        else{
-            console.log("Datos incorrectos ingresados")
-            document.getElementById("errorS").innerHTML='<p style="color: red;">DATOS INCORRECTOS</p>';
+        const nickname = document.getElementById("nicknameU").value;
+        const password = document.getElementById("contraU").value;
+
+        if (nickname === user1 && password === pass1) {
+            console.log("Datos correctos");
+            document.getElementById("errorS").innerHTML = '<p style="color: green;">DATOS CORRECTOS</p>';
+            window.alert("Sesión iniciada ¡Bienvenido!");
+            window.location.href = 'Inicio/inicio.html';
+        } else if (nickname.trim() === "" || password.trim() === "") {
+            console.log("Completa los campos");
+            document.getElementById("errorS").innerHTML = '<p style="color: red;">COMPLETA AMBOS CAMPOS</p>';
+        } else {
+            console.log("Datos incorrectos ingresados");
+            document.getElementById("errorS").innerHTML = '<p style="color: red;">DATOS INCORRECTOS</p>';
         }
     } catch (error) {
-        console.log("Se ha producido un error")
+        console.log("Se ha producido un error", error);
     }
-};
+}
