f4cdaae957dba8e1e658e7749aa98f7926528005
Brian <163011832+brx2b@users.noreply.github.com>
2025-06-24 13:40:21 -0400

    Add files via upload

--- Estado/EstadoRep.js
+++ Estado/EstadoRep.js
@@ -1,8 +1,10 @@
-const { createClient } = require('@supabase/supabase-js');
-
-const supabaseUrl = 'https://pyevuwdwubtepetyltnu.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5ZXZ1d2R3dWJ0ZXBldHlsdG51Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0NzY2MTYsImV4cCI6MjA2NjA1MjYxNn0.LstnF5yWE9cPH4bs_y-9ojaFru7R69Yo1o5y58mdnFc'
-const supabase = createClient(supabaseUrl, supabaseKey)
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js"></script>
+window.supabaseClient = supabase.createClient(
+    //url brx
+    'https://pyevuwdwubtepetyltnu.supabase.co',
+    //key
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5ZXZ1d2R3dWJ0ZXBldHlsdG51Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0NzY2MTYsImV4cCI6MjA2NjA1MjYxNn0.LstnF5yWE9cPH4bs_y-9ojaFru7R69Yo1o5y58mdnFc'
+);
 
   // Prueba simple: listar los datos de una tabla (por ejemplo, 'usuarios')
   async function testConnection() {
